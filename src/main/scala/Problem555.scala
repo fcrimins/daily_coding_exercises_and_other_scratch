920fd73b9354745eefd67a9ffddce36b2c8fb4da
Frederick Crimins <fcrimins@gmail.com>
2016-11-14 16:07:37 -0500

    Exception in thread "main" java.lang.StackOverflowError at M_mks.apply(Problem555.scala:30)

--- src/main/scala/Problem555.scala
+++ src/main/scala/Problem555.scala
@@ -1,27 +1,26 @@
-
-import org.apache.log4j._
+import org.apache.logging.log4j._
 
 /**
   * https://projecteuler.net/problem=555
   * Created by fred on 11/11/16.
   */
 object Problem555 {
-  @transient lazy val logger = Logger.getLogger(getClass.getName
+  /*@transient lazy*/ val logger = LogManager.getLogger
 
   def main(args: Array[String]): Unit = {
+
     val m = 100
     val M_91 = new M_mks(m, 11, 10)
     for(n <- 1 to m) {
-      println("M_91(" + n + ") = " + M_91(n))
+      logger.info("M_91(" + n + ") = " + M_91(n))
     }
 
     val F_mks = (1 to m).filter(n => n == M_91(n))
-    println("F_mks = " + F_mks)
-    println("SF_mks = " + F_mks.sum)
+    logger.info("F_mks = " + F_mks)
+    logger.info("SF_mks = " + F_mks.sum)
 
-    val S_10_10 = new S_pm(1000, 1000)
-    println("S_10_10() = " + S_10_10())
-
+    val S_10_10 = new S_pm(10e6.asInstanceOf[Int], 10e6.asInstanceOf[Int])
+    logger.info("S_10_10() = " + S_10_10())
   }
 }
 
