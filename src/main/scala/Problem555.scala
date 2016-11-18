52773caa6bdf2bb1d04aa7d63a02f6ad0a2bec19
Frederick Crimins <fcrimins@gmail.com>
2016-11-18 08:34:45 -0500

    Complete!

--- src/main/scala/Problem555.scala
+++ src/main/scala/Problem555.scala
@@ -12,6 +12,11 @@ object Problem555 {
 
   def main(args: Array[String]): Unit = {
 
+    val s0: BigInt = Range.BigInt(250000, 357143, 1).sum
+    val s1: BigInt = (250000 until 357143).sum
+    logger.info(s"s0=$s0, s1=$s1")
+    //System.exit(1)
+
     val m = 100
     val k = 9
     val s = 2
@@ -26,9 +31,6 @@ object Problem555 {
     logger.info("F_mks=" + F_mks)
     logger.info("SF_mks=" + F_mks.sum)
 
-    // http://www.scala-lang.org/old/node/9604
-    //collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(4)
-
     logger.info("S_10_10()=" + new S_pm(10, 10)()) // 225
     logger.info("S_1000_1000()=" + new S_pm(1000, 1000)()) // 208724467
     logger.info("S_10000_10000()=" + new S_pm(10000, 10000)()) // 208541582207
@@ -37,7 +39,7 @@ object Problem555 {
     val S_1e_1e = new S_pm(pm, pm)
     val cumsum = S_1e_1e()
     logger.info("S_1e_1e()D=" + S_1e_1e.cumsumD)
-    logger.info("S_1e_1e()=" + cumsum) // 1343978449270432 (wrong :(
+    logger.info("S_1e_1e()=" + cumsum) // 208517717451208352 (odd how they all start w/ 2s, I wonder if there's a pattern akin to that of prime numbers)
   }
 }
 
@@ -78,7 +80,7 @@ class M_mks(m: Int, k: Int, s: Int) {
     recursive_apply(apply_n_plus_ks)
   }
 
-  def apply(n: Int): Int = {
+  def apply(n: BigInt): BigInt = {
     // the extra "+ k - s" is to ensure that the left operand is positive
     val i_ = (n - argmin) % ks
     val i = if (i_ < 0) i_ + ks else i_
@@ -102,31 +104,55 @@ class S_pm(p: Int, m: Int) {
 
   def apply(): BigInt = {
 
+    // Range.BigInt has exclusive end
     val s_range = Range.BigInt(1, p+1, 1).par
+    //val s_range = Range.BigInt(750001, 750002, 1).par
+
+    // http://docs.scala-lang.org/overviews/parallel-collections/configuration.html
     s_range.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(14))
 
     val cumsums = s_range.map { (s: BigInt) => {
       if (s % 1000 == 0)
         logger.info(s"_s=$s")
-      val cs: BigInt = (s.toInt+1 to p).map { k => {
+
+      val k_range = Range.BigInt(s+1, p+1, 1) // not parallel
+
+      val foldinit: BigInt = 0
+      val cs: BigInt = k_range.fold(foldinit) { (foldcum: BigInt, k: BigInt) => {
         //logger.info("k=" + k)
-        val M = new M_mks(m, k, s.toInt)
+        val M = new M_mks(m, k.toInt, s.toInt)
 
         // F_{m,k,s} is the set of fixed points of M_{m,k,s}
         // no reason to iterate over the entire (1 to m) when we're only looking
         // for values equal to one of the preprocessed values
         //val SF_mks = (1 to m).filter(n => n == M(n)).sum
 
-        // there are only 2 options: (1) M can be constant, e.g. 91, or (2) it can
-        // be equal to itself for every element over the [min,max] range
+        // there are only 3 options: (1) M can be constant, e.g. 91, (2) it can
+        // be equal to itself for the first element and every element over the
+        // [min,max] range, or (3) it can not coincide ever with its input
         var SF_mks: BigInt = 0
-        if (M.min_val == M.max_val) // <- implies that both are <m
+        if (M.min_val == M.max_val) {
           SF_mks = M.max_val
-        else if (M.min_val == M(M.min_val))
-          SF_mks = (M.min_val to M.max_val).sum
-        SF_mks
+          //logger.info(s"s=$s k=$k v=${M.max_val} minmax")
+        }
+        else if (M.min_val == M(M.min_val)) {
+          val N: BigInt = M.max_val - M.min_val + 1
+          SF_mks = N * (M.min_val - 1) + N * (N + 1) / 2 // don't factor out N to avoid odd N+1
+          //SF_mks = Range.BigInt(M.min_val, M.max_val+1, 1).sum
+          //SF_mks = (M.min_val to M.max_val).sum // <- int overflow
+          //logger.info(s"s=$s k=$k v=${M.min_val}-${M.max_val}")
+          //(M.min_val to M.max_val).foreach(v => logger.info(s"s=$s k=$k v=$v"))
+        }
 
-      }}.sum
+        /*if (SF_mks != 0)
+          logger.info(s"k=$k for SF_mks=$SF_mks")*/
+        foldcum + SF_mks
+      }}
+
+      if (cs < 0) {
+        logger.fatal(s"cs=$cs for s=$s, p=$p, m=$m")
+        System.exit(1)
+      }
 
       if (p == 1e6)
         logger.info(s"s=$s, cs=$cs")
