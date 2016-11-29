fac57ed24122d3a8f19d498478885f6e15e5af8e
Frederick Crimins <fcrimins@gmail.com>
2016-11-29 07:40:49 -0500

    Still wrong answer but much faster!

--- build.sbt
+++ build.sbt
@@ -5,12 +5,16 @@ name := "projecteuler_scala"
 
 version := "1.0"
 
-scalaVersion := "2.12.0"
+scalaVersion := "2.11.8"
 
 // library dependencies: (groupId) % (artifactId) % (version)
 libraryDependencies ++= Seq(
+  //"org.scala-lang" % "scala-library" % scalaVersion.value % "compile",
   "org.apache.logging.log4j" % "log4j-api" % "2.7",
-  "org.apache.logging.log4j" % "log4j-core" % "2.7"
+  "org.apache.logging.log4j" % "log4j-core" % "2.7",
+  "org.apache.spark" % "spark-core_2.11" % "2.0.0", // would be nice to "Getting the right Scala version with %%"
+  "org.apache.spark" % "spark-mllib_2.11" % "2.0.0", // per here http://www.scala-sbt.org/1.0/docs/Library-Dependencies.html
+  "org.apache.spark" % "spark-streaming_2.11" % "2.0.0"
   //"org.apache.commons" % "commons-math3" % "3.1.1",
   //"org.fluentd" % "fluent-logger" % "0.2.10",
   //"org.mockito" % "mockito-core" % "1.9.5" % "test"  // Test-only dependency
--- src/main/scala/Problem557.scala
+++ src/main/scala/Problem557.scala
@@ -1,10 +1,7 @@
-import java.io._
-
 import org.apache.logging.log4j._
-
-import scala.collection.immutable.Range
-import scala.collection.parallel._
-import scala.collection.parallel.immutable.ParSeq
+import org.apache.spark.sql._
+import org.apache.spark.sql.Encoder
+import org.apache.spark.sql.expressions.Aggregator
 
 /**
   * https://projecteuler.net/problem=557
@@ -15,32 +12,58 @@ object Problem557 {
 
   def main(args: Array[String]): Unit = {
 
-    val min_n: BigInt = 4
-    val max_n: BigInt = 10000
+    val num_drivers = 24
+    val spark = SparkSession.builder
+      .master(s"local[$num_drivers]") // using `nxy`: 1e3 -> 8:32/8threads, 5:46/16, 5:02/24, 4:31/32, 4:55/40
+      .appName(getClass.getSimpleName) // new! using `nxa`: 1e3 -> 30s / 24 threads
+      .getOrCreate
+    import spark.implicits._
 
-    //val n = (4 to max_n)
-    //val w = (1 to 2*n)
-    //val nw = n.cross(w)
+    val min_n = 4
+    val max_n = 1e4.toInt // 10->12, 20->259, 1e2->20765, 1e3->8736041, 1e4->5015484425?
 
-    val n_range = Range.BigInt(min_n, max_n+1, 1).par
-    n_range.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(14))
+    // initial uncut triangle search range for n = a + b + c + d
+    val ni = (min_n to max_n by num_drivers)
+    val ns = (0 until num_drivers).flatMap(offset => ni.map(n => n + offset))
+    val n_range: Dataset[Int] = spark.createDataset(ns.filter(_ <= max_n))
 
-    //n_range.foreach { n => logger.info(s"n=$n") }
+    // join n to x, y, and a
+    val nx = n_range.flatMap(n => (2 to n-2).map((n, _))) // x = a + b
+//star
+    val nxa = nx.flatMap { case (n, x) => (1 to x-1).map((n, x, _)) }
+    val valid_quadruple = nxa.map { case (n, x, a) => {
 
-    val Ss: ParSeq[BigInt] = n_range.map { (n: BigInt) => {
-      //if (n % 100 == 1)
-        logger.info(s"n=$n")
+      val b = x - a
 
-      if (n > max_n) {
-        logger.error(s"Why is $n > $max_n?")
-        System.exit(1)
-      }
+      // d = numer / denom * (c + a) - b
+      val numer: BigInt = b * n // BigInt already has a gcd method defined
+      val denom = a * x
+      val g = (numer gcd denom).toInt
 
-      var num_n = 0
-      for(x <- 2 to n.toInt - 2; // a+b
-          y <- x to n.toInt - 2; // a+c
-          a <- 1 to x - 1) {
-        //val h = 2*n.toDouble / w
+      // d will be an integer at c's periodicity of p (for c starting at -a, i.e. c=-a,
+      // c=-a+p, c=-a+2p, ...)
+      val p = denom / g
+
+      // this is where c+a gets divided by p
+      def d(c: Int): Int = numer.toInt * (c + a) / denom - b
+
+      // number of n's at which c and d are integers (it's possible that the first
+      // couple c's are non-positive)
+      val num_n = (-a to n - x - 1 by p).count(c => (c >= b) && (d(c) + c == n - x))
+      //if (num_n > 0) logger.info(s"n=$n numer=$numer denom=$denom g=$g p=$p a=$a b=$b cs=" + (-a to n - x - 1 by p).filter(c => (c >= b) && (d(c) + c == n - x)))
+      if (num_n > 0) logger.info(s"n=$n numer=$numer denom=$denom g=$g p=$p a=$a b=$b num_n=$num_n")
+      /*val cs = (-a to n - x - 1 by p).filter(c => (c >= b) && (d(c) + c == n - x))
+      cs.foreach { c => logger.info(s"n=$n numer=$numer denom=$denom g=$g p=$p  a=$a b=$b c=$c d=${d(c)}") }*/
+      num_n * n
+/*
+    val nxy = nx.flatMap { case (n, x) => (x to n-2).map((n, x, _)) } // y = a + c
+    //val nxya = nxy.flatMap { case (n, x, y) => (1 to x-1).map((n, x, y, _)) }
+
+    val valid_quadruple = nxy.map { case (n, x, y) => {
+      // sum over all the possible valus of `a` given n, x, and y (= a + c)
+      // 10->2 of these, 20->17, 1e2->320, 1e3->14270
+      val sum_a = (1 to x-1).fold(0) { (s, a) => {
+
         val b = x - a
         val c = y - a
         val d = n - a - b - c
@@ -55,24 +78,41 @@ object Problem557 {
         //    between dc and ba
         // 5) but they aren't colinear and as one line rotates away from
         //    the other (using ca's vertex as a pivot point) d grows at
-        //    a rate proportional to the ratio n:a+b (= 1 + d+c:a+b)
+        //    faster than c a rate equal to the ratio n:a+b
 
-        val r1 = (d.toDouble + b) / (c + a)
-        val r2 = (b.toDouble * n.toInt) / (a + b) / a
+        val r1 = (d.toDouble + b) / (a + c)
+        val r2 = b.toDouble / a * n.toInt / (a + b)
 
-        if ((r1 - r2).abs < 1e-10) {
-          num_n += 1
-          //logger.info(f"n=$n, ($a, $b, $c, $d), r2=$r1, r2=$r2")
-          //if (n % 100 == 1)
-            //logger.info(s"n=$n, foldcum=$foldcum, num_n=$num_n")
-        }
-      }
+        if ((r1 - r2).abs < 1e-10) logger.info(f"n=$n, ($a, $b, $c, $d), r1=$r1, r2=$r2")
+        if ((r1 - r2).abs < 1e-10) s + n else s
+      }}
 
-      n * num_n
+      if (sum_a != 0)
+        logger.info(f"n=$n, x=$x, y=$y, s=$sum_a")
+      sum_a
+*/
     }}
 
-    val S: BigInt = Ss.sum
+    //valid_quadruple.repartition(32) // 1e3 -> 11:22/16threads
+
+    // https://docs.cloud.databricks.com/docs/spark/1.6/examples/Dataset%20Aggregator.html
+    val bigIntSum = new Aggregator[Int, Long, Long] with Serializable {
+      def zero: Long = 0                     // The initial value.
+      def reduce(b: Long, a: Int) = b + a    // Add an element to the running total
+      def merge(b1: Long, b2: Long) = b1 + b2 // Merge intermediate values.
+      def finish(b: Long) = b
+
+      // "there currently is no real good support for custom class encoders"
+      // http://stackoverflow.com/questions/36648128/how-to-store-custom-objects-in-a-dataset
+      override def bufferEncoder: Encoder[Long] = Encoders.scalaLong
+      override def outputEncoder: Encoder[Long] = Encoders.scalaLong
+    }.toColumn
+
+    val S: Long = valid_quadruple.select(bigIntSum).collect()(0)
+    //val S = valid_quadruple.reduce((s: Int, n: Int) => s + n)
     logger.info(f"S = $S")
+
+    spark.stop
   }
 
   /**
@@ -80,6 +120,19 @@ object Problem557 {
     */
   implicit class Crossable[X](xs: Traversable[X]) {
     def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
+  }
+
+  /**
+    * http://stackoverflow.com/questions/295579/fastest-way-to-determine-if-an-integers-square-root-is-an-integer
+    */
+  def isSquare(n: Long): Boolean = {
+    if (n < 0) {
+      false
+    }
+    else {
+      val tst: Long = (Math.sqrt(n) /*+ 0.5*/).toLong
+      tst * tst == n
+    }
   }
 }
 
